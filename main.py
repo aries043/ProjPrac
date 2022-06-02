d65fcf2219a39521d91a6b2c8d16332d0cd305ea
Chanwoo Kim <84301139+aries043@users.noreply.github.com>
2022-06-02 15:18:02 +0900

    Extraction of Excel based on desired vehicle type

--- main.py
+++ main.py
@@ -22,3 +22,33 @@ excel_source = pd.read_excel('C:/Users/cksdn/PycharmProjects/OSS_Project_04/resu
 print("Please enter the type of car you want. ex) 1\n"
       "1.SM3 Z.E, 2.레이EV, 3.소울EV, 4.닛산리프, 5.아이오닉EV, 6. BMW i3, 7.스파크EV, 8.볼트EV, 9.테슬라")
 car = input()
+
+if car == "1" :
+    str_want_go = "SM3 Z.E"
+    int_line = excel_source['지원차종'].str.contains(str_want_go)
+elif car == "2" :
+    str_want_go = "레이EV"
+    int_line = excel_source['지원차종'].str.contains(str_want_go)
+elif car == "3" :
+    str_want_go = "소울EV"
+    int_line = excel_source['지원차종'].str.contains(str_want_go)
+elif car == "4" :
+    str_want_go = "닛산리프"
+    int_line = excel_source['지원차종'].str.contains(str_want_go)
+elif car == "5" :
+    str_want_go = "아이오닉EV"
+    int_line = excel_source['지원차종'].str.contains(str_want_go)
+elif car == "6" :
+    str_want_go = "BMW i3"
+    int_line = excel_source['지원차종'].str.contains(str_want_go)
+elif car == "7" :
+    str_want_go = "스파크EV"
+    int_line = excel_source['지원차종'].str.contains(str_want_go)
+elif car == "8" :
+    str_want_go = "볼트EV"
+    int_line = excel_source['지원차종'].str.contains(str_want_go)
+elif car == "9" :
+    str_want_go = "테슬라"
+    int_line = excel_source['지원차종'].str.contains(str_want_go)
+want_go_excel = excel_source[int_line]
+want_go_excel.to_excel('C:/Users/cksdn/PycharmProjects/OSS_Project_04/result1.xlsx', sheet_name= 'Result')
