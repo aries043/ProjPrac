e50fbdfa85e2188dbb114a78eb84ec1e0d1c649e
Chanwoo Kim <84301139+aries043@users.noreply.github.com>
2022-06-02 15:06:39 +0900

    Import pandas, get excel file, get region input

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ import chargingMethod
 import returnAddress
 import findCurloc
 import findDestination
+import pandas as pd
 
 chargingMethod.guide()
+
+excel_source = pd.read_excel('C:/Users/cksdn/PycharmProjects/OSS_Project_04/전기차-충전소-설치현황_20220316.xlsx', usecols=[1, 2, 3, 4, 5])
+print("Please enter the area where you want to find the location of the charging station. ex)청주")
+str_want_go = input()
