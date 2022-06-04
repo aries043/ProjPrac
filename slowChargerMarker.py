b7562baeb9437577d76500ee9a0d5c4c1f402ed5
Chanwoo Kim <84301139+aries043@users.noreply.github.com>
2022-06-04 21:45:22 +0900

    Design icon, add feature cluster

--- slowChargerMarker.py
+++ slowChargerMarker.py
@@ -3,15 +3,20 @@ import returnAddress
 import folium as g
 from folium.plugins import MarkerCluster
 
-
 def slowChargerMarker(g_map):
     excel_source = pd.read_excel('C:/Users/cksdn/PycharmProjects/OSS_Project_04/result2.xlsx', usecols=[2])
     lat = []
     lng = []
-for count in range(excel_source.shape[0]):
+    for count in range(excel_source.shape[0]):
         chg_lat, chg_lng = returnAddress.getloc(str(excel_source.loc[count]))
         lat.append(chg_lat)
         lng.append(chg_lng)
     locations = list(zip(lat, lng))
     icons = [g.Icon(icon='glyphicon glyphicon-flash',
-                    popup='Charging Station') for _ in range(len(locations))]
+                    popup='Charging Station',
+                    prefix="glyphicon",
+                    color='gray',
+                    icon_color='#FFFF00') for _ in range(len(locations))]
+
+    cluster = MarkerCluster(locations = locations, icons = icons)
+    cluster.add_to(g_map)
