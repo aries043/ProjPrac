4c116edc08bc06a3623ff435df90faa608677550
Chanwoo Kim <84301139+aries043@users.noreply.github.com>
2022-06-04 21:43:01 +0900

    Read Excel, make 2 list

--- slowChargerMarker.py
+++ slowChargerMarker.py
@@ -2,3 +2,9 @@ import pandas as pd
 import returnAddress
 import folium as g
 from folium.plugins import MarkerCluster
+
+
+def slowChargerMarker(g_map):
+    excel_source = pd.read_excel('C:/Users/cksdn/PycharmProjects/OSS_Project_04/result2.xlsx', usecols=[2])
+    lat = []
+    lng = []
