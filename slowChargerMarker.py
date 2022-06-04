64bc12b2f7d67bd1e7ce352250e3ef7756be379b
Chanwoo Kim <84301139+aries043@users.noreply.github.com>
2022-06-04 21:43:24 +0900

    Make lat, lng

--- slowChargerMarker.py
+++ slowChargerMarker.py
@@ -8,3 +8,7 @@ def slowChargerMarker(g_map):
     excel_source = pd.read_excel('C:/Users/cksdn/PycharmProjects/OSS_Project_04/result2.xlsx', usecols=[2])
     lat = []
     lng = []
+for count in range(excel_source.shape[0]):
+        chg_lat, chg_lng = returnAddress.getloc(str(excel_source.loc[count]))
+        lat.append(chg_lat)
+        lng.append(chg_lng)
